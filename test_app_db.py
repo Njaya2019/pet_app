01a08f505bbd97afbe394c391edd5ff38ce0e39f
Andrew <njayaandrew@gmail.com>
2018-08-05 03:39:49 +0300

    insert pet name to table pets

--- test_app_db.py
+++ test_app_db.py
@@ -1,12 +1,31 @@
 import pytest, json
 from application import app
 from app_db import PetAPIdb
+import MySQLdb
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def cli():
     client=app.test_client()
     return client
+
+@pytest.fixture(scope="module")
+def cur():
+    petApi=PetAPIdb()
+    con=petApi.connectDB()
+    cur=con.cursor()
+    yield cur
+    cur.close()
+    con.close()
+
+def test_name(cur):
+    cur.execute("SELECT pet_name FROM pets WHERE id= %s" % (1))
+    name=cur.fetchone()
+    assert name[0]=="Obama"
+
+
+    
+
 
 def test_post(cli):
     response=cli.post('/petsdb',data=json.dumps(dict(pet_name="Raila")),content_type="application/json")
