e0fe5abd887a51f80eaac3701473c44090fca774
Andrew <njayaandrew@gmail.com>
2018-08-28 02:25:13 +0300

    post test

--- test_app_db.py
+++ test_app_db.py
@@ -9,7 +9,6 @@ def cli():
 def test_post(cli):
     response=cli.post('/petsdb',data=json.dumps(dict(pet_name="Raila")),content_type="application/json")
     data = json.loads(response.data)
-    assert response.status_code==200
     assert "pet created" in data["message"]
 
 
