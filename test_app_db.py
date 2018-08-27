833892fd41e638e19b23c5c5b9073827fe40ef9c
Andrew <njayaandrew@gmail.com>
2018-08-28 02:19:20 +0300

    post test

--- test_app_db.py
+++ test_app_db.py
@@ -6,14 +6,6 @@ import MySQLdb
 def cli():
     client=app.test_client()
     return client
-@pytest.fixture(scope="module")
-def cur():
-    petApi=PetAPIdb()
-    con=petApi.connectDB()
-    cur=con.cursor()
-    yield cur
-    cur.close()
-    con.close()
 def test_post(cli):
     response=cli.post('/petsdb',data=json.dumps(dict(pet_name="Raila")),content_type="application/json")
     data = json.loads(response.data)
